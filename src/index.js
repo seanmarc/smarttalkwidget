060d8873aaef0dad52af1877d5599feb696ffa00
seanmarc <seanmarcusjoesting+1@gmail.com>
2023-06-08 17:02:08 -0400

    fix: timing when appending to root

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// Create a new div element
-const root = document.createElement('div');
+document.addEventListener("DOMContentLoaded", function() {
+  // Create a new div element
+  const root = document.createElement('div');
 
-// Append it to the body of the document
-document.body.appendChild(root);
+  // Append it to the body of the document
+  document.body.appendChild(root);
 
-// Now render the App component into the new div
-ReactDOM.createRoot(root).render(<App />);
+  // Now render the App component into the new div
+  ReactDOM.createRoot(root).render(<App />);
+});
